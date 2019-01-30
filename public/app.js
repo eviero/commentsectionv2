ed11bb92d3cc56ed07f7ce3d930d967d72d212ba
eviero <33431795+eviero@users.noreply.github.com>
2019-01-29 19:46:13 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -73,6 +73,8 @@ $(function(){
 //     var aComment = '<p>"' + data.items[randomNum].snippet.topLevelComment.snippet.textDisplay + '"</p>'
 
       $('body').css({
+        'text-align':'left',
+        'padding-left':'10',
         'font-family':'Courier',
         'font-size': '25',
         'background-color':'black'
