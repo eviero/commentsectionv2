dcd8bbab2d42f9afd3bd54f3df2c1db89621d3ef
eviero <33431795+eviero@users.noreply.github.com>
2019-01-29 21:50:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 var request = require('request'); //use the request package to make api requests
 var videoID = ['dQw4w9WgXcQ', 'FZUcpVmEHuk']
+var randomVideo = videoID[Math.floor(Math.random() * videoID.length)]
 
 app.use(express.static('public')) //server out the public files statically!
 
@@ -10,7 +11,7 @@ app.use(express.static('public')) //server out the public files statically!
 app.get('/youtube', function(req,res){
 
   // use the request package to make a call to the forecast API
-  request('https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyAxCSKrqlW6u9-7WXaA1xvjRJoxkZd8wog&textFormat=plainText&part=snippet&videoId=' + videoID[Math.floor(Math.random() * videoID.length)] + '&maxResults=100', function(error, response, body){
+  request('https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyAxCSKrqlW6u9-7WXaA1xvjRJoxkZd8wog&textFormat=plainText&part=snippet&videoId=' + randomVideo + '&maxResults=100', function(error, response, body){
     // console.log(error)
     // if(error){ console.error(error); return;}
     // console.log( JSON.parse(body) );
@@ -23,4 +24,14 @@ app.get('/youtube', function(req,res){
 var port = process.env.PORT || 3000
 app.listen(port, function(){
   console.log("running on 3k")
+})
+
+$(function(){
+
+randomVideo = videoID[Math.floor(Math.random() * videoID.length)]
+
+      setInterval(function(){ 
+  randomVideo = Math.floor(Math.random() * data.items.length)
+}, 5000)
+
 })
